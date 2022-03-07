4ade3ef91b045ed6c07c7ab70d328594f165906f
avadhut <avadhut.talbar@example.com>
2022-03-06 20:03:58 -0600

    Added asg3

---
+++ ADB-Assignment3/WebApp/settings_template.py
@@ -0,0 +1,11 @@
+server = 'EnterServerURL'
+database = 'EnterDBName'
+username = 'EnterUserName'
+password = 'EnterPassword'   
+driver= 'Enter Driver'
+
+mapQuest_key = 'EnterKey'
+mapQuest_url = 'http://open.mapquestapi.com/geocoding/v1/address?key='
+
+myRedisHostname = "YourHostName"
+myRedisPassword = "YourAccessKey"
